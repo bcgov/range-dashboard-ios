6d747a7831151235c6e6d64806a771b0702a4822
Jason C. Leach <jason.leach@fullboar.ca>
2019-06-04 09:48:47 -0700

    Add missing file

---
+++ MyRangeBCManager/SSOConstants.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+struct SSO {
+    static let baseUrl = URL(string: "https://sso.pathfinder.gov.bc.ca")!
+    static let redirectUri = "xxx-xxx://client"
+    static let clientId = "xxx-xxx"
+    static let realmName = "xxx"
+    static let idpHint = "idir"
+}
